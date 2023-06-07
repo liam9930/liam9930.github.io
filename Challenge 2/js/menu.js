3006813167a6820729a91c5fd11f170126253094
weitzai <liam9930@gmail.com>
2023-06-07 23:15:10 +0800

    much update

--- Challenge 2/js/menu.js
+++ Challenge 2/js/menu.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
-    // 收合選單
-  $('.dropdown').click(function(e){ 
+  // 收合選單
+  $('.dropdown').click(function (e) {
     e.preventDefault();
-    $('.dropdown-open').slideToggle();
-    // 點擊到這個元素回到父層再到父層內找子元素 .dropdown-open進行 slideToggle 這動作
+    //burger-menu的交換icon部分與滿版的部分
+    // .dropdown-open進行 toggleClass('menuShow') 加入這個class的動作
+    $('.dropdown-open').toggleClass('menuShow');
+    $('.menu-btn').toggleClass('btn-open');
+    $('.menu-btn').toggleClass('btn-close');
   });
+
+  //AiTool由新到舊的部分
+  // 開起選單
+  $('.dropdown-btn').click(function (e) {
+    $('.dropdown-menu').toggleClass('show');
+  });
+
+  // 切換按鈕文字
+  $('.new-to-old').click(function (e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text("由新到舊");
+  });
+
+  $('.old-to-new').click(function (e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text("由舊到新");
+  });
+
+
+
+
+
 });
